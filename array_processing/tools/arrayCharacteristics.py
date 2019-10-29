1ffaed951c2ecc59c2ac0b017dad133580968945
Liam Toney <ldtoney@alaska.edu>
2019-10-29 10:49:29 -0800

    Remove call that relies on a LaTeX install

--- array_processing/tools/arrayCharacteristics.py
+++ array_processing/tools/arrayCharacteristics.py
@@ -6,11 +6,11 @@ arrayCharacteristics
 Provides
     1. Array characteristic methods applicable to geophysical sesnor arrays
     2. Support functions for array characteristic calculations
-    
+
 How to use the module
 ---------------------
-Documentation is available in docstrings provided with the code. The 
-docstring examples assume that `arrayCharacteristics` has been imported 
+Documentation is available in docstrings provided with the code. The
+docstring examples assume that `arrayCharacteristics` has been imported
 as `arrChar`::
 
     import arrayCharacteristics as arrChar
@@ -60,8 +60,8 @@ All of the methods in this module are written for use under Python 3.*
 
 """
 
-# Archival note: per PEP 396, for a module with version-numbered methods, 
-# the module has no version number.  Each method in this module has a 
+# Archival note: per PEP 396, for a module with version-numbered methods,
+# the module has no version number.  Each method in this module has a
 # version-number embedded as an attribute per PEP 440 as (e.g.):
 #       method.__version__ = '3.2.1'
 
@@ -77,12 +77,12 @@ Created on Wed Sep 18 13:02:15 2019
 wolf = '\U0001F43A'
 print(wolf)
 
-def arraySig(rij, kmax, sigLevel, p=0.9, velLims=(0.27, 0.36), NgridV=100, 
+def arraySig(rij, kmax, sigLevel, p=0.9, velLims=(0.27, 0.36), NgridV=100,
              NgridTh=100, NgridK=100):
     r"""
-    Estimate 2D array uncertainties in trace velocity and back azimuth, 
+    Estimate 2D array uncertainties in trace velocity and back azimuth,
     calculate impulse response
-    
+
     Parameters
     ----------
     rij :   array
@@ -95,25 +95,25 @@ def arraySig(rij, kmax, sigLevel, p=0.9, velLims=(0.27, 0.36), NgridV=100,
     p : float
         Confidence limit in uncertainty estimates (optional, default is 0.9)
     velLims : tuple of float(s)
-        Range of trace velocities (km/s) to estimate uncertainty over, single 
+        Range of trace velocities (km/s) to estimate uncertainty over, single
         value can be used (optional, default is (0.27, 0.36))
     NgridV : int
-        Number of velocities to estimate uncertainties in range `velLims` 
+        Number of velocities to estimate uncertainties in range `velLims`
         (optional, default is 100)
     NgridTh : int
-        Number of angles to estimate uncertainties in range :math:`[0^\circ, 
+        Number of angles to estimate uncertainties in range :math:`[0^\circ,
         360^\circ)` (optional, default is 100)
     NgridK : int
         Number of k-space coordinates to calculate in each dimension (optional,
         default is 100)
-        
+
     Returns
-    -------   
+    -------
     sigV : array
-        Uncertainties in trace velocity :math:`(^\circ)` as a function of trace 
+        Uncertainties in trace velocity :math:`(^\circ)` as a function of trace
         velocity and back azimuth as (NgridTh, NgridV) array
     sigTh : array
-        Uncertainties in trace velocity (km/s) as a function of trace velocity 
+        Uncertainties in trace velocity (km/s) as a function of trace velocity
         and back azimuth as (NgridTh, NgridV) array
     impResp : array
         Impulse response over grid as (NgridK, NgridK) array
@@ -123,8 +123,8 @@ def arraySig(rij, kmax, sigLevel, p=0.9, velLims=(0.27, 0.36), NgridV=100,
         Vector of back azimuths :math:`(^\circ)` for axis in (NgridTh, ) array
     kvec : array
         Vector wavenumbers for axes in k-space in (NgridK, ) array
-    
-            
+
+
     Version
     -------
     2.4 -- 20 Sep 2019
@@ -133,12 +133,12 @@ def arraySig(rij, kmax, sigLevel, p=0.9, velLims=(0.27, 0.36), NgridV=100,
     arraySig.__version__ = '2.4'
     # (c) 2019 Curt A. L. Szuberla
     # University of Alaska Fairbanks, all rights reserved
-    
+
     from numpy import (linspace, pi, meshgrid, sin, cos, zeros, arctan2, array,
                        sqrt, abs, diff)
     from numpy.linalg import eig
-                       
-    
+
+
     # calculate uncertainties
     # preliminaries
     dij = co_array(rij)
@@ -188,7 +188,7 @@ def arraySig(rij, kmax, sigLevel, p=0.9, velLims=(0.27, 0.36), NgridV=100,
 def impulseResp(dij, kmax, NgridK):
     """
     Calculate impulse response of a 2D array
-    
+
     Parameters
     ~~~~~~~~~~
     dij : array
@@ -198,14 +198,14 @@ def impulseResp(dij, kmax, NgridK):
         in k-space
     NgridK : int
         Number of k-space coordinates to calculate in each dimension
-        
+
     Returns
-    ~~~~~~~   
+    ~~~~~~~
     d : array
         Impulse response over grid as (NgridK, NgridK) array
     kvec : array
         Vector wavenumbers for axes in k-space in (NgridK, ) array
-            
+
     Version
     ~~~~~~~
     1.0 -- 20 Sep 2019
@@ -214,10 +214,10 @@ def impulseResp(dij, kmax, NgridK):
     impulseResp.__version__ = '1.0'
     # (c) 2019 Curt A. L. Szuberla
     # University of Alaska Fairbanks, all rights reserved
-    
+
     from numpy import linspace, meshgrid, vstack, cos, sum, reshape, sqrt
-    
-    
+
+
     # pre-allocate grid for k-space
     kvec  = linspace(-kmax, kmax, NgridK)
     Kx, Ky = meshgrid(kvec, kvec)
@@ -233,7 +233,7 @@ def rthEllipse(a, b, x0, y0):
     """
     Calculate angles subtending, and extremal distances to, a coordinate-
     aligned ellipse from the origin
-    
+
     Parameters
     ~~~~~~~~~~
     a : float
@@ -246,28 +246,28 @@ def rthEllipse(a, b, x0, y0):
         vertical center of ellipse
 
     Returns
-    ~~~~~~~   
+    ~~~~~~~
     eExtrm : array
         Extremal parameters in (4, ) array as
         [min distance, max distance, min angle (degrees), max angle (degrees)]
     eVec : array
         Coordinates of extremal points on ellipse in (4, 2) array as
-        [[x min dist., y min dist.], [x max dist., y max dist.], 
-         [x max angle tangency, y max angle tangency], 
+        [[x min dist., y min dist.], [x max dist., y max dist.],
+         [x max angle tangency, y max angle tangency],
          [x min angle tangency, y min angle tangency]]
-        
+
     Version
     ~~~~~~~
     1.1.1 -- 7 Oct 2019
-    
+
     """
     rthEllipse.__version__ = '1.1.1'
     # (c) 2019 Curt A. L. Szuberla
     # University of Alaska Fairbanks, all rights reserved
-    
+
     from numpy import (zeros, abs, finfo, float64, array, sqrt, where, real,
                        vstack, max, hstack, sort, arctan2, pi)
-    
+
     # set constants
     A = 2/a**2
     B = 2*x0/a**2
@@ -280,7 +280,7 @@ def rthEllipse(a, b, x0, y0):
     eExtrm = zeros((4, ))
     eVec = zeros((4, 2))
     eps = finfo(float64).eps
-    
+
     # some tolerances for numerical errors
     circTol = 1e8  # is it circular to better than circTol*eps?
     zeroTol = 1e4  # is center along a coord. axis to better than zeroTol*eps?
@@ -308,7 +308,7 @@ def rthEllipse(a, b, x0, y0):
             # use dual solutions of quartics to find best, real-valued results
             # solve quartic for y
             fy = F**2*H
-            y = quarticEqn(-D*F*(2*H+F)/fy, (B**2*(G+F)+E*F**2+D**2*(H+2*F))/fy, 
+            y = quarticEqn(-D*F*(2*H+F)/fy, (B**2*(G+F)+E*F**2+D**2*(H+2*F))/fy,
                            -D*(B**2+2*E*F+D**2)/fy, (D**2*E)/fy)
             y = array([y[i] for i in list(where(y == real(y))[0])])
             xy = B*y / (D-F*y)
@@ -324,7 +324,7 @@ def rthEllipse(a, b, x0, y0):
                     )
             # trap real, but bogus sol's (esp. near Th = 180)
             distEidx = where(
-                (distE <= sqrt(x0**2 + y0**2)+max([a, b])*(1+magTol)) & 
+                (distE <= sqrt(x0**2 + y0**2)+max([a, b])*(1+magTol)) &
                 (distE >= sqrt(x0**2 + y0**2)-max([a, b])*(1+magTol))
                 )
             coords = hstack(((x, yx), (xy, y))).T
@@ -354,7 +354,7 @@ def rthEllipse(a, b, x0, y0):
 def co_array(rij):
     """
     Form co-array coordinates for given array coordinates
-    
+
     Parameters
     ~~~~~~~~~~
     rij : array
@@ -362,20 +362,20 @@ def co_array(rij):
         column vectors in `d` dimensions
 
     Returns
-    ~~~~~~~   
+    ~~~~~~~
     dij : array
         (d, n(n-1)//2) co-array, coordinates of the sensor pairing separations
-        
+
     Version
     ~~~~~~~
     1.0 -- 13 Feb 2017
-    
+
     """
     co_array.__version__ = '1.0'
     # (c) 2017 Curt A. L. Szuberla
     # University of Alaska Fairbanks, all rights reserved
     #
-    idx = [(i, j) for i in range(rij.shape[1]-1) 
+    idx = [(i, j) for i in range(rij.shape[1]-1)
                     for j in range(i+1,rij.shape[1])]
     return rij[:,[i[0] for i in idx]] - rij[:,[j[1] for j in idx]]
 
@@ -385,29 +385,29 @@ def chi2(nu, alpha, funcTol=1e-10):
     Calculate value of a :math:`\chi^2` such that a :math:`\nu`-dimensional
     confidence ellipsoid encloses a fraction :math:`1 - \alpha` of normally
     distributed variable
-    
+
     Parameters
     ~~~~~~~~~~
-    nu : int 
+    nu : int
         degrees of freedom (typically embedding dimension of variable)
     alpha : float
         confidence interval such that :math:`\alpha \in [0, 1]`
     funcTol : float (optional)
         optimzation function evaluation tolerance for :math:`\nu \ne 2`,
         defaults to 1e-10
-    
+
     Returns
     ~~~~~~~
     chi2val : float
-        value of a :math:`\chi^2` enclosing :math:`1 - \alpha` confidence 
+        value of a :math:`\chi^2` enclosing :math:`1 - \alpha` confidence
         region
-        
+
     Version
     ~~~~~~~
     1.0 -- 7 Oct 2019
 
     """
-    
+
     chi2.__version__ = '1.0'
     # (c) 2019 Curt A. L. Szuberla
     # University of Alaska Fairbanks, all rights reserved
@@ -415,13 +415,13 @@ def chi2(nu, alpha, funcTol=1e-10):
     if nu == 2:
         from numpy import log
         # this shorthand owing to Ken Arnoult
-        return -2 * log(alpha) 
+        return -2 * log(alpha)
     else:
         # but just in case we end up with a nu != 2 situation
         from scipy import optimize
-        from scipy.special import gammainc 
+        from scipy.special import gammainc
         from numpy import abs
-        gammaTest = lambda X2test: abs(gammainc(nu/2, 
+        gammaTest = lambda X2test: abs(gammainc(nu/2,
                                                 X2test/2) - (1-alpha))
         return optimize.fmin(func=gammaTest, x0=1, ftol=funcTol, disp=False)
 
@@ -429,29 +429,29 @@ def chi2(nu, alpha, funcTol=1e-10):
 def quadraticEqn(a,b,c):
     r"""
     Roots of quadratic equation in the form :math:`ax^2 + bx + c = 0`
-    
+
     Parameters
     ~~~~~~~~~~
-    a, b, c : int or float, can be complex 
+    a, b, c : int or float, can be complex
         Scalar coefficients of quadratic equation in standard form
-        
+
     Returns
     ~~~~~~~
     x : list
         Roots of quadratic equation in standard form
-         
+
     See Also
     ~~~~~~~~
     numpy.roots : generic polynomial root finder
-    
+
     Notes
     ~~~~~
-    1) Stable solutions, even for :math:`b^2 >> ac` or complex coefficients, 
+    1) Stable solutions, even for :math:`b^2 >> ac` or complex coefficients,
     per algorithm of NR 2d ed. :math:`\S` 5.6.
-    
+
     Version
     ~~~~~~~
-    1.0.1 -- 10 Feb 2017 
+    1.0.1 -- 10 Feb 2017
 
     """
     quadraticEqn.__version__ = '1.0.1'
@@ -489,30 +489,30 @@ def quadraticEqn(a,b,c):
 def cubicEqn(a,b,c):
     """
     Roots of cubic equation in the form :math:`x^3 + ax^2 + bx + c = 0`
-    
+
     Parameters
     ~~~~~~~~~~
-    a, b, c : int or float, can be complex 
+    a, b, c : int or float, can be complex
         Scalar coefficients of cubic equation in standard form
-        
+
     Returns
     ~~~~~~~
     x : list
         Roots of cubic equation in standard form
-         
+
     See Also
     ~~~~~~~~
     numpy.roots : generic polynomial root finder
-    
+
     Notes
     ~~~~~
-    1) Relatively stable solutions, with some tweaks by Dr. Z, per algorithm 
-    of NR 2d ed. :math:`\S` 5.6.  Even np.roots can have some (minor) issues; 
+    1) Relatively stable solutions, with some tweaks by Dr. Z, per algorithm
+    of NR 2d ed. :math:`\S` 5.6.  Even np.roots can have some (minor) issues;
     e.g., :math:`x^3 - 5x^2 + 8x - 4 = 0`.
-    
+
     Version
     ~~~~~~~
-    1.0.1 -- 11 Feb 2017 
+    1.0.1 -- 11 Feb 2017
 
     """
     # (c) 2017 Curt A. L. Szuberla
@@ -520,7 +520,7 @@ def cubicEqn(a,b,c):
     #
     cubicEqn.__version__ = '1.0.1'
     from numpy import isreal, sqrt, real
-    Q = a*a/9 - b/3 
+    Q = a*a/9 - b/3
     R = (3*c - a*b)/6 + a*a*a/27
     Q3 = Q*Q*Q
     R2 = R*R
@@ -565,8 +565,8 @@ def cubicEqn(a,b,c):
         else:
             A = -(R-sqR2mQ3)**(1/3)
         if A==0:
-            B = 0 
-        else: 
+            B = 0
+        else:
             B = Q/A
         # one real root & two conjugate complex ones
         x = [
@@ -585,31 +585,31 @@ def cubicEqn(a,b,c):
 
 def quarticEqn(a,b,c,d):
     """
-    Roots of quartic equation in the form :math:`x^4 + ax^3 + bx^2 + 
+    Roots of quartic equation in the form :math:`x^4 + ax^3 + bx^2 +
     cx + d = 0`
-    
+
     Parameters
     ~~~~~~~~~~
-    a, b, c, d : int or float, can be complex 
+    a, b, c, d : int or float, can be complex
         Scalar coefficients of quartic equation in standard form
-        
+
     Returns
     ~~~~~~~
     x : list
         Roots of quartic equation in standard form
-         
+
     See Also
     ~~~~~~~~
     numpy.roots : generic polynomial root finder
-    
+
     Notes
     ~~~~~
-    1) Stable solutions per algorithm of CRC  Std. Mathematical 
+    1) Stable solutions per algorithm of CRC  Std. Mathematical
     Tables, 29th ed.
-    
+
     Version
     ~~~~~~~
-    1.0.1 -- 11 Feb 2017 
+    1.0.1 -- 11 Feb 2017
 
     """
     # (c) 2017 Curt A. L. Szuberla
@@ -622,7 +622,7 @@ def quarticEqn(a,b,c,d):
     y = cubicEqn(-b, a*c - 4*d, (4*b - a2)*d - c*c)
     y = y[0]
     # find R
-    R = sqrt(a2/4 - (1+0j)*b + y) # force complex in sqrt 
+    R = sqrt(a2/4 - (1+0j)*b + y) # force complex in sqrt
     foo = 3*a2/4 - R*R - 2*b
     if R != 0:
         D = sqrt(foo + (a*b - 2*c - a2*a/4)/R) # R is already complex here
@@ -649,7 +649,7 @@ def quarticEqn(a,b,c,d):
 def arraySigPlt(rij, sig, sigV, sigTh, impResp, vel, th, kvec, figName=None):
     """
     Plots output of arraySig method
-    
+
     Parameters
     ----------
     rij : array
@@ -657,10 +657,10 @@ def arraySigPlt(rij, sig, sigV, sigTh, impResp, vel, th, kvec, figName=None):
     sigLevel : float
         Variance in time delays (s), typically :math:`\sigma_\tau`
     sigV : array
-        Uncertainties in trace velocity :math:`(^\circ)` as a function of trace 
+        Uncertainties in trace velocity :math:`(^\circ)` as a function of trace
         velocity and back azimuth as (NgridTh, NgridV) array
     sigTh : array
-        Uncertainties in trace velocity (km/s) as a function of trace velocity 
+        Uncertainties in trace velocity (km/s) as a function of trace velocity
         and back azimuth as (NgridTh, NgridV) array
     impResp : array
         Impulse response over grid as (NgridK, NgridK) array
@@ -672,19 +672,18 @@ def arraySigPlt(rij, sig, sigV, sigTh, impResp, vel, th, kvec, figName=None):
         Vector wavenumbers for axes in k-space in (NgridK, ) array
     figName : str
         Name of output file, will be written as figName.png (optional)
-  
+
     """
-    
+
     # this is just a sanity check, so no real docstring
     arraySigPlt.__version__ = '1.0'
-    
+
     # std. (if inefficient) plotting import block
     import matplotlib.pyplot as plt
-    
+
     # for plotting methods & scripts
     figFormat = 'png'       # MUCH faster than pdf!!
     figDpi = 600               # good resolution
-    plt.rc('text', usetex=True)
 
     # lower RHS is array geometry
     axRij = plt.subplot(2, 2, 4)
@@ -694,14 +693,14 @@ def arraySigPlt(rij, sig, sigV, sigTh, impResp, vel, th, kvec, figName=None):
     plt.ylabel('km')
     axRij.axis('square')
     axRij.grid()
-    
+
     # upper RHS is impulse reponse
     axImp = plt.subplot(2, 2, 2)
     plt.pcolormesh(kvec, kvec, impResp)
     plt.ylabel('k$_y$ (km$^{-1}$)')
     plt.xlabel('k$_x$ (km$^{-1}$)')
     axImp.axis('square')
-    
+
     # upper RHS is th uncertainty
     plt.subplot(2, 2, 1)
     meshTh = plt.pcolormesh(th, vel, sigTh)
@@ -718,7 +717,7 @@ def arraySigPlt(rij, sig, sigV, sigTh, impResp, vel, th, kvec, figName=None):
     plt.xlabel(r'$\theta (\circ)$')
     cbrV = plt.colorbar(meshV, )
     cbrV.set_label('$\delta v$')
-    
+
     # prepare output & display in iPython workspace
     plt.tight_layout() # IGNORE renderer warning from script! it does just fine
     if figName is not None:
@@ -729,14 +728,14 @@ def arraySigContourPlt(sigV, sigTh, vel, th, trace_v):
     """
     Plots output of arraySig method onto a polar plot for a specified trace
     velocity.
-    
+
     Parameters
     ----------
     sigV : array
-        Uncertainties in trace velocity :math:`(^\circ)` as a function of trace 
+        Uncertainties in trace velocity :math:`(^\circ)` as a function of trace
         velocity and back azimuth as (NgridTh, NgridV) array
     sigTh : array
-        Uncertainties in trace velocity (km/s) as a function of trace velocity 
+        Uncertainties in trace velocity (km/s) as a function of trace velocity
         and back azimuth as (NgridTh, NgridV) array
     vel : array
         Vector of trace velocities (km/s) for axis in (NgridV, ) array
@@ -744,13 +743,13 @@ def arraySigContourPlt(sigV, sigTh, vel, th, trace_v):
         Vector of back azimuths :math:`(^\circ)` for axis in (NgridTh, ) array
     trace_v : float
         Specified trace velocity (km/s) for uncertainy plot
-        
+
     Returns
     ~~~~~~~
     fig : figure handle
-    
+
     author: D. Fee
-   
+
     """
     import matplotlib.pyplot as plt
     import numpy as np
@@ -759,25 +758,25 @@ def arraySigContourPlt(sigV, sigTh, vel, th, trace_v):
     sigV_cont = sigV[tvel_ptr,:]
     sigTh_cont = sigTh[tvel_ptr,:]
     theta = np.linspace(0, 2 * np.pi, len(sigV_cont))
-    
-    
+
+
     fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, subplot_kw={'projection': 'polar'})
-    
+
     ax1.set_theta_direction(-1)
     ax1.set_theta_offset(np.pi/2.0)
     ax1.plot(theta, sigV_cont, color='k', lw=1)
     ax1.set_rmax(sigV_cont.max()*1.1)
     ax1.yaxis.get_major_locator().base.set_params(nbins=6)
-    ax1.set_rlabel_position(22.5)  
+    ax1.set_rlabel_position(22.5)
     ax1.grid(True)
     ax1.set_title('Trace Velocity Uncertainty, V=%.2f' % trace_v, va='bottom', pad=20)
-    
+
     ax2.set_theta_direction(-1)
     ax2.set_theta_offset(np.pi/2.0)
     ax2.plot(theta, sigTh_cont, color='b', lw=1)
     ax2.set_rmax(sigTh_cont.max()*1.1)
     ax2.yaxis.get_major_locator().base.set_params(nbins=6)
-    ax2.set_rlabel_position(22.5)  
+    ax2.set_rlabel_position(22.5)
     ax2.grid(True)
     ax2.set_title('Back-Azimuth Uncertainty, V=%.2f' % trace_v, va='bottom', pad=20)
 
