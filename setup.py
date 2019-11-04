ea7e35dd65054bfcb5441c5e65bfa593c7216dd4
Liam Toney <ldtoney@alaska.edu>
2019-11-03 17:06:20 -0900

    Fix linter complaints

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-config = {'name':               'array_processing',
-          'url':                'https://github.com/uafgeotools/array_processing',
-          'packages':           find_packages(),
-          'install_requires':   'waveform_collection'
-         }
+config = {'name':             'array_processing',
+          'url':              'https://github.com/uafgeotools/array_processing',
+          'packages':         find_packages(),
+          'install_requires': 'waveform_collection'
+          }
 
 setup(**config)
