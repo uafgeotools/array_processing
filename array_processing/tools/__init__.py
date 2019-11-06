1ffea85e98ea76c45eafd0bbf3603a644f30023f
Liam Toney <ldtoney@alaska.edu>
2019-11-05 16:14:02 -0900

    Linter fix

--- array_processing/tools/__init__.py
+++ array_processing/tools/__init__.py
@@ -2,6 +2,6 @@ from .array_characterization import (arraySig, impulseResp, rthEllipse,
                                      co_array, chi2, cubicEqn, quadraticEqn,
                                      quarticEqn)
 from .detection import MCCMcalc, fstatbland
-from .generic import (array_thresh, beamForm, phaseAlignData, phaseAlignIdx, tauCalcPW,
-                      tauCalcSW, tauCalcSWxy, randc, psf, srcLoc)
+from .generic import (array_thresh, beamForm, phaseAlignData, phaseAlignIdx,
+                      tauCalcPW, tauCalcSW, tauCalcSWxy, randc, psf, srcLoc)
 from .plotting import array_plot, arraySigPlt, arraySigContourPlt
