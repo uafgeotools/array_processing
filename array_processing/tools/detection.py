c14c8c29d8a7218c2a9ad3668a3dc9dd7a8e4188
Liam Toney <ldtoney@alaska.edu>
2020-02-27 09:56:05 -0900

    Docstring edits: detection module

--- array_processing/tools/detection.py
+++ array_processing/tools/detection.py
@@ -4,44 +4,48 @@ from .generic import phaseAlignIdx, phaseAlignData
 
 
 def MCCMcalc(cmax, wgt=None):
-    """
-    Weighted mean (and median) of the cross-correlation maxima of wlsqva
-
-    This method calculates `MCCM` as the weighted arithmetic mean of the cross-
-    correlation maxima output of `wlsqva`, such that channels given a weight
-    identical to zero are excluded from the `MCCM` calculation.  Optionally, it
-    will calculate `MdCCM` as the weighted median of same.
+    r"""
+    Weighted mean (and median) of the cross-correlation maxima of
+    :func:`~array_processing.algorithms.wlsqva.wlsqva`. This method
+    calculates MCCM as the weighted arithmetic mean of the cross-
+    correlation maxima output of
+    :func:`~array_processing.algorithms.wlsqva.wlsqva`, such that channels
+    given a weight identical to zero are excluded from the MCCM calculation.
+    Optionally, it will calculate MdCCM as the weighted median of same.
 
     Args:
-        cmax : array
-               (n(n-1)//2, ) cross-correlation maxima for `n` channels of data and
-               each channel pairing in `tau` (output of `wlsqva`)
-        wgt : array
-               (n, ) weights corresponding each of the `n` data channels (input to
-               `wlsqva`). Default is an array of ones (all channels equally
-               weighted and included).
+        cmax: ``(n(n-1)//2, )`` array; cross-correlation maxima for ``n``
+            channels of data and each channel pairing in `tau` (output of
+            :func:`~array_processing.algorithms.wlsqva.wlsqva`)
+        wgt: ``(n, )`` array; weights corresponding each of the ``n`` data
+            channels (input to
+            :func:`~array_processing.algorithms.wlsqva.wlsqva`). Default is an
+            array of ones (all channels equally weighted and included)
 
     Returns:
-        MCCM : float
-               Weighted arithmetic mean of the cross-correlation maxima
-        MdCCM : float
-               Weighted median of the cross-correlation maxima
+        tuple: Tuple containing:
 
-    Notes
-    ~~~~~
-    A typical use of the weights in `wlsqva` is a simple binary scheme (i.e.,
-    zeros to exclude channels & ones to include).  If both `cmax` and `wgt`
-    are passed to `MCCMcalc`, then the unweighted channels will not
-    contribute to the value `MCCM`.  If, however, only `cmax` is passed in such
-    a case, the value of `MCCM` will be lower than expected.  This is since
-    `wlsqva` sets identically to zero any `cmax` element corresponding to a
-    pairing with an unweighted channel.  If `wgt` was passed to `wlsqva`, use
-    it in this method, too, in order to ensure the expected numerical behavior
-    of `MCCM`.  Non-binary weights in `wgt` will be used to calculate the
-    traditionally-defined, weighted arithmetic mean of `cmax`.
+        - **MCCM** – Weighted arithmetic mean of the cross-correlation maxima
+        - **MdCCM** – Weighted median of the cross-correlation maxima
 
-    In the calculation of `MdCCM`, the weights are converted to binary -- i.e,
-    channels are simply either included or excluded.
+    Notes:
+        A typical use of the weights in
+        :func:`~array_processing.algorithms.wlsqva.wlsqva` is a simple binary
+        scheme (i.e., zeros to exclude channels & ones to include). If both
+        `cmax` and `wgt` are passed to this function, then the unweighted
+        channels will not contribute to the value `MCCM`. If, however, only
+        `cmax` is passed in such a case, the value of `MCCM` will be lower
+        than expected. This is since
+        :func:`~array_processing.algorithms.wlsqva.wlsqva` sets identically to
+        zero any `cmax` element corresponding to a pairing with an
+        unweighted channel. If `wgt` was passed to
+        :func:`~array_processing.algorithms.wlsqva.wlsqva`, use it in this
+        method, too, in order to ensure the expected numerical behavior of
+        `MCCM`. Non-binary weights in `wgt` will be used to calculate the
+        traditionally-defined, weighted arithmetic mean of `cmax`.
+
+        In the calculation of `MdCCM`, the weights are converted to binary —
+        i.e, channels are simply either included or excluded.
     """
 
     if wgt is None:
@@ -62,27 +66,26 @@ def MCCMcalc(cmax, wgt=None):
 
     return MCCM, MdCCM
 
+
 def fstatbland(dtmp, fs, tau):
-    """
-    Calculates the F-statistic and SNR based on Blandford's method.
+    r"""
+    Calculates the F-statistic and SNR based on Blandford's method (see Notes).
 
     Args:
-        dtmp : array
-            (m, n) time series with `m` samples from `n` traces as columns
-        fs : float or int
-            sample rate [Hz]
-        tau : array
-            (n(n-1)//2) time delays of relative signal arrivals (TDOA) for all
-            unique sensor pairings
+        dtmp: ``(m, n)`` array; time series with ``m`` samples from ``n``
+            traces as columns
+        fs (int or float): Sample rate [Hz]
+        tau: ``(n(n-1)//2)`` array; time delays of relative signal arrivals
+            (TDOA) for all unique sensor pairings
 
     Returns:
-        fstat : array
-            F-statistic
-        snr : float
-            SNR
+        tuple: Tuple containing:
 
-    Reference:
-      Blandford, R. R., 1974, Geophysics, vol. 39, no. 5, p. 633-643
+        - **fstat** – F-statistic
+        - **snr** – SNR
+
+    Notes:
+        Blandford, R. R., 1974, Geophysics, vol. 39, no. 5, p. 633–643.
     """
 
     m, n = dtmp.shape
@@ -110,17 +113,18 @@ def fstatbland(dtmp, fs, tau):
 
     return fstat, snr
 
+
 def calculate_semblance(data_in):
-    """
+    r"""
     Calculates the semblance, a measure of multi-channel coherence, following
-    the defintion of Neidell & Taner [1971. Assume data are already
+    the definition of Neidell & Taner (1971). Assumes data are already
     time-shifted to construct the beam.
 
     Args:
-        data: time-shifted Stream or time-shifted numpy array
+        data_in: Time-shifted ObsPy Stream or time-shifted NumPy array
 
     Returns:
-        semblance: [0-1] Multi-channel coherence
+        Multi-channel coherence defined on :math:`[0, 1]`
     """
 
     if isinstance(data_in, Stream):
