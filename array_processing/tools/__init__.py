b7f5249a96aae73b31f7cc15ba4d69f592cb289c
Jordan W. Bishop <36087082+jwbishop@users.noreply.github.com>
2020-03-19 07:11:56 -0800

    Remove obsolete MCCMcalc fxn (#28)
    * Remove obsolete MCCMcalc fxn
    
    * Remove MCCMcalc from init file

--- array_processing/tools/__init__.py
+++ array_processing/tools/__init__.py
@@ -1,7 +1,7 @@
 from .array_characterization import (arraySig, impulseResp, rthEllipse,
                                      co_array, chi2, cubicEqn, quadraticEqn,
                                      quarticEqn)
-from .detection import MCCMcalc, fstatbland
+from .detection import fstatbland
 from .generic import (array_thresh, beamForm, phaseAlignData, phaseAlignIdx,
                       tauCalcPW, tauCalcSW, tauCalcSWxy, randc, psf)
 from .plotting import array_plot, arraySigPlt, arraySigContourPlt
