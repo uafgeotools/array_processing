ec879fde147358759153b5dd0a07a1d13cdd1e92
jwbishop <jwbishop2@alaska.edu>
2023-01-10 17:04:47 -0900

    Fix example.py due to LTS update

--- example.py
+++ example.py
@@ -15,7 +15,7 @@ END = START + 20*60
 
 # Filtering
 FMIN = 0.1  # [Hz]
-FMAX = 1    # [Hz]
+FMAX = 1.0    # [Hz]
 
 # Array processing
 WINLEN = 50  # [s]
@@ -31,41 +31,35 @@ st.taper(max_percentage=0.01)
 
 #%% Array processing and plotting using least squares
 
-from array_processing.algorithms.helpers import getrij
 from array_processing.tools.plotting import array_plot
 from lts_array import ltsva
 
 latlist = [tr.stats.latitude for tr in st]
 lonlist = [tr.stats.longitude for tr in st]
 
-rij = getrij(latlist, lonlist)
-
 #%% Array processing. ALPHA = 1.0: least squares processing.
 ALPHA = 1.0
-vel, baz, t, mdccm, stdict, sig_tau = ltsva(st, rij, WINLEN, WINOVER, ALPHA)
+vel, baz, t, mdccm, stdict, sigma_tau, conf_int_vel, conf_int_baz = ltsva(st, latlist, lonlist, WINLEN, WINOVER, ALPHA)
 
 fig1, axs1 = array_plot(st, t, mdccm, vel, baz, ccmplot=True, mcthresh=0.6,
-                        sigma_tau=sig_tau)
+                        sigma_tau=sigma_tau)
 
 #%% Array processing. 0.5 <= ALPHA < 1.0: least trimmed squares processing.
-ALPHA_LTS = 0.75
-vel_lts, baz_lts, t_lts, mdccm_lts, stdict_lts, sig_tau_lts = ltsva(st, rij,
-                                                                    WINLEN,
-                                                                    WINOVER,
-                                                                    ALPHA_LTS)
+ALPHA_LTS = 0.50
+vel_lts, baz_lts, t_lts, mdccm_lts, stdict_lts, sigma_tau, conf_int_vel, conf_int_baz = ltsva(st, latlist, lonlist, WINLEN, WINOVER, ALPHA_LTS)
 
-fig1_lts, axs1_lts = array_plot(st, t_lts, mdccm_lts, vel_lts, baz_lts,
-                                ccmplot=True, mcthresh=0.6, sigma_tau=None,
-                                stdict=stdict_lts)
+fig1_lts, axs1_lts = array_plot(st, t_lts, mdccm_lts, vel_lts, baz_lts, ccmplot=True, mcthresh=0.6, sigma_tau=None,  stdict=stdict_lts)
 
 #%% Array uncertainty
-
+from array_processing.algorithms.helpers import getrij
 from array_processing.tools import arraySig
 from array_processing.tools.plotting import arraySigPlt, arraySigContourPlt
 
 SIGLEVEL = 1/st[0].stats.sampling_rate
 KMAX = 400
 TRACE_VELOCITY = 0.33
+rij = getrij(latlist, lonlist)
+
 
 sigV, sigTh, impResp, vel, th, kvec = arraySig(rij, kmax=KMAX,
                                                sigLevel=SIGLEVEL)
